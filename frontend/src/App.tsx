3fa7243208d53e90b8291c3f461f495c1e71a0e0
ejminguez <erroljamesminguez59@gmail.com>
2026-06-25 02:33:37 +0800

    feat: use createBrowserRouter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,6 @@
-import { Routes, Route } from "react-router-dom";
-import { AppLayout } from "@/components/layout/AppLayout";
-import CorridorMonitor from "@/pages/CorridorMonitor";
-import RoutesPage from "@/pages/RoutesPage";
-import IncidentsPage from "@/pages/IncidentsPage";
-import AnalyticsPage from "@/pages/AnalyticsPage";
-import SettingsPage from "@/pages/SettingsPage";
+import { RouterProvider } from "react-router-dom";
+import { router } from "@/router";
 
 export default function App() {
-  return (
-    <Routes>
-      <Route element={<AppLayout />}>
-        <Route index element={<CorridorMonitor />} />
-        <Route path="routes" element={<RoutesPage />} />
-        <Route path="incidents" element={<IncidentsPage />} />
-        <Route path="analytics" element={<AnalyticsPage />} />
-        <Route path="settings" element={<SettingsPage />} />
-      </Route>
-    </Routes>
-  );
+  return <RouterProvider router={router} />;
 }
